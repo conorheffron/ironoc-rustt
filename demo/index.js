bd51c9cedef01fa9d39299fc2bb051d8268faa03
Conor H <conor.heffron@gmail.com>
2024-10-14 06:18:53 +0100

    #1 Refactor generated package (rename)

--- demo/index.js
+++ demo/index.js
@@ -1,16 +1,16 @@
 // Import our outputted wasm ES6 module
 // Which, export default's, an initialization function
-import wasmInit from "./pkg/hello_world.js";
+import wasmInit from "./pkg/demo.js";
 
 const runWasm = async () => {
   // Instantiate our wasm module
-  const helloWorld = await wasmInit("./pkg/hello_world_bg.wasm");
+  const helloWorld = await wasmInit("./pkg/demo_bg.wasm");
 
   // Call the Add function export from wasm, save the result
   const addResult = helloWorld.add(24, 24);
 
   // Set the result onto the body
-  document.body.textContent = `Hello World! addResult: ${addResult}`;
+  document.body.textContent = `Hello World Demo! addResult: ${addResult}`;
 };
 runWasm();
 
